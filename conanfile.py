420a4ad90d61b0406bef4301a075b7a319ba9521
arnor-nolen <andrei.gorbulin@rightware.com>
2023-08-31 14:54:50 +0300

    Black formatter

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@ from conan.tools.files import copy
 from conan.tools.files import replace_in_file
 from conan.tools.cmake import cmake_layout, CMakeToolchain
 
+
 class VulkanTutorialConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps"
@@ -18,7 +19,7 @@ class VulkanTutorialConan(ConanFile):
     }
 
     def requirements(self):
-        assert(self.requires is not None)
+        assert self.requires is not None
 
         self.requires("vulkan-headers/1.3.224.0", override=True)
         self.requires("sdl/2.28.2")
@@ -31,32 +32,45 @@ class VulkanTutorialConan(ConanFile):
         self.requires("volk/1.3.224.0")
         self.requires("lz4/1.9.3")
         self.requires("nlohmann_json/3.11.2")
-        self.requires("spdlog/1.10.0")  # Any version >= 1.9.0 crashes on MacOS (might be fixed by now, see https://github.com/conan-io/conan-center-index/issues/8480)
+        self.requires(
+            "spdlog/1.10.0"
+        )  # Any version >= 1.9.0 crashes on MacOS (might be fixed by now, see https://github.com/conan-io/conan-center-index/issues/8480)
         self.requires("fmt/9.1.0", override=True)
         self.requires("wayland/1.22.0", override=True)  # Version 1.21 segfaults
         self.requires("libxml2/2.11.4", override=True)  # For Wayland
         # "msdf-atlas-gen/1.2.2",
 
     def generate(self):
-        assert(self.source_folder is not None)
+        assert self.source_folder is not None
 
         # Copy ImGui headers
-        copy(self,
-             "*sdl2*", 
-             src=os.path.join(self.dependencies["imgui"].package_folder, "res", "bindings"),
-             dst=os.path.join(self.source_folder, "src", "bindings"),
-             keep_path=False
+        copy(
+            self,
+            "*sdl2*",
+            src=os.path.join(
+                self.dependencies["imgui"].package_folder, "res", "bindings"
+            ),
+            dst=os.path.join(self.source_folder, "src", "bindings"),
+            keep_path=False,
         )
 
-        copy(self,
-             "*vulkan*", 
-             src=os.path.join(self.dependencies["imgui"].package_folder, "res", "bindings"),
-             dst=os.path.join(self.source_folder, "src", "bindings"),
-             keep_path=False
+        copy(
+            self,
+            "*vulkan*",
+            src=os.path.join(
+                self.dependencies["imgui"].package_folder, "res", "bindings"
+            ),
+            dst=os.path.join(self.source_folder, "src", "bindings"),
+            keep_path=False,
         )
 
         # Fix HIDPI issue with SDL2 bindings
-        replace_in_file(self, os.path.join(self.source_folder, "src", "bindings", "imgui_impl_sdl2.cpp"), "SDL_GetWindowSize", "SDL_GetWindowSizeInPixels")
+        replace_in_file(
+            self,
+            os.path.join(self.source_folder, "src", "bindings", "imgui_impl_sdl2.cpp"),
+            "SDL_GetWindowSize",
+            "SDL_GetWindowSizeInPixels",
+        )
 
         # Copy shared libraries
         copy(self, "*.dll", src=".", dst="bin", keep_path=False)
@@ -67,6 +81,6 @@ class VulkanTutorialConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.user_presets_path = "ConanPresets.json"
         tc.generate()
-        
+
     def layout(self):
         cmake_layout(self)
